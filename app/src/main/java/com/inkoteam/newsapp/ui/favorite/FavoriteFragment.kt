1dd474855486be8e4eded85a247ef12d9929a409
Inkoteam <inkoteam@inbox.ru>
2022-06-13 00:36:45 +0300

    Lesson-2

---
+++ app/src/main/java/com/inkoteam/newsapp/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,26 @@
+package com.inkoteam.newsapp.ui.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.inkoteam.newsapp.R
+import com.inkoteam.newsapp.databinding.FragmentDetailsBinding
+import com.inkoteam.newsapp.databinding.FragmentFavoriteBinding
+
+
+class FavoriteFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+}
